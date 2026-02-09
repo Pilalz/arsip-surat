49076dab03962cd76400380410f88f2962c43ec6
Pilal <rafihilalzahir@gmail.com>
2026-02-09 16:59:18 +0700

    mail config lagi

--- config/mail.php
+++ config/mail.php
@@ -43,18 +43,12 @@ return [
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
+            'encryption' => env('MAIL_ENCRYPTION', null), // Tambahkan ini
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-
-            'context' => [
-                'ssl' => [
-                    'allow_self_signed' => true,
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                ],
-            ],
+            'verify_peer' => false, // Tambahkan ini untuk disable SSL verification
 
             'stream' => [
                 'ssl' => [
